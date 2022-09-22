a5003845bcdb0e59e4ef9b31883e32a35270f563
rayder773 <gerasymenkoden@gmail.com>
2022-09-22 15:31:46 +0200

    check

--- main.js
+++ main.js
@@ -4,24 +4,7 @@ import { Frame } from "./Frame.js";
 const component = new Component({
   viewModel: {
     div: {
-      text: "hello world",
       name: "main",
-      // append: [
-      //   {
-      //     div: {
-      //       name: "child_1",
-      //       text: "child 1",
-      //       append: [
-      //         {
-      //           span: {
-      //             name: "child_1_1",
-      //             text: "child_1_1",
-      //           },
-      //         },
-      //       ],
-      //     },
-      //   },
-      // ],
     },
   },
   events: {
@@ -49,12 +32,13 @@ const child = new Component({
 component.create();
 component.setEvents();
 
-// debugger;
-component.change({
-  main: {
-    append: [child.viewModel],
-  },
-});
+// component.add({});
+// // debugger;
+// component.change({
+//   main: {
+//     append: [child.viewModel],
+//   },
+// });
 
 app.append(component.html);
 
